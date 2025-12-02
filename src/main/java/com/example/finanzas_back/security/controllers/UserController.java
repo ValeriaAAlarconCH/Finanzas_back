07288860d659cf0f98e15815079a842320d8e1d1
ValeriaAAlarconCH <u20221d218@upc.edu.pe>
2025-12-02 12:47:19 -0500

    seguridad

---
+++ src/main/java/com/example/finanzas_back/security/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.example.finanzas_back.security.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import com.example.finanzas_back.security.entities.User;
+import com.example.finanzas_back.security.services.UserService;
+
+//@CrossOrigin(origins = "${ip.frontend}")
+@CrossOrigin(origins = "${ip.frontend}", allowCredentials = "true", exposedHeaders = "Authorization") //para cloud
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder bcrypt;
+
+    @PostMapping("/user")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void createUser(@RequestBody User user) {
+        String bcryptPassword = bcrypt.encode(user.getPassword());
+        user.setPassword(bcryptPassword);
+        userService.save(user);
+    }
+    @PostMapping("/save/{user_id}/{rol_id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Integer> saveUseRol(@PathVariable("user_id") Long user_id,
+                                              @PathVariable("rol_id") Long rol_id){
+        return new ResponseEntity<Integer>(userService.insertUserRol(user_id, rol_id), HttpStatus.OK);
+        //return new ResponseEntity<Integer>(uService.insertUserRol2(user_id, rol_id),HttpStatus.OK);
+    }
+}
+
